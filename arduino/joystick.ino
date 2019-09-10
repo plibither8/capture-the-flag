2c93258e7826937fa262703d552f942cad15cb49
Mihir Chaturvedi <plibither8@gmail.com>
2019-09-10 22:49:57 +0530

    Style: Fix code style

--- arduino/joystick.ino
+++ arduino/joystick.ino
@@ -1,15 +1,12 @@
-void setup()
-{
+void setup() {
 	// put your setup code here, to run once:
 	pinMode(A0, INPUT);
 	pinMode(A1, INPUT);
 	Serial.begin(9600);
 }
 
-void loop()
-{
-	struct Input
-	{
+void loop() {
+	struct Input {
 		int x;
 		int y;
 	};
@@ -20,24 +17,22 @@ void loop()
 	analogInput.x = analogRead(A0);
 	analogInput.y = analogRead(A1);
 
-	switch (analogInput.x)
-	{
-	case 0:
-		Serial.println("Left:");
-		break;
-	case 1023:
-		Serial.println("Right:");
-		break;
+	switch (analogInput.x) {
+		case 0:
+			Serial.println("Left:");
+			break;
+		case 1023:
+			Serial.println("Right:");
+			break;
 	}
 
-	switch (analogInput.y)
-	{
-	case 0:
-		Serial.println("Up:");
-		break;
-	case 1023:
-		Serial.println("Down:");
-		break;
+	switch (analogInput.y) {
+		case 0:
+			Serial.println("Up:");
+			break;
+		case 1023:
+			Serial.println("Down:");
+			break;
 	}
 
 	delay(50);

14760de470a618178629908d155dae8a544fc125
Mihir Chaturvedi <plibither8@gmail.com>
2019-09-21 21:22:31 +0530

    Fix: Refine arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,8 @@
 void setup() {
   pinMode(A0, INPUT); // Joystick X
   pinMode(A1, INPUT); // Joystick Y
+  pinMode(A2, INPUT); // Joystick X
+  pinMode(A3, INPUT); // Joystick Y
   pinMode(2, INPUT); // Button
   digitalWrite(2, HIGH);
 
@@ -14,30 +16,52 @@ void loop() {
     int y;
   };
 digitalWrite(2, HIGH);
-  struct Input analogInput;
+  struct Input analogInput1, analogInput2;
 
-  analogInput.x = analogRead(A0);
-  analogInput.y = analogRead(A1);
+  analogInput1.x = analogRead(A0);
+  analogInput1.y = analogRead(A1);
+  analogInput2.x = analogRead(A2);
+  analogInput2.y = analogRead(A3);
 //
-//  switch (analogInput.x) {
-//    case 0:
-//      Serial.println("Left0:");
-//      break;
-//    case 1023:
-//      Serial.println("Right0:");
-//      break;
-//  }
-//
-//  switch (analogInput.y) {
-//    case 0:
-//      Serial.println("Up0:");
-//      break;
-//    case 1023:
-//      Serial.println("Down0:");
-//      break;
-//  }
+switch (analogInput1.x) {
+case 0:
+  Serial.println("Left0:");
+  break;
+case 1023:
+  Serial.println("Right0:");
+  break;
+}
 
-//  if (digitalRead(2) == LOW) {
-//    Serial.println("Enter:");
-//  }
+switch (analogInput2.x) {
+case 0:
+  Serial.println("Left1:");
+  break;
+case 1023:
+  Serial.println("Right1:");
+  break;
+}
+
+
+switch (analogInput1.y) {
+case 0:
+  Serial.println("Up0:");
+  break;
+case 1023:
+  Serial.println("Down0:");
+  break;
+}
+
+switch (analogInput2.y) {
+case 0:
+  Serial.println("Up1:");
+  break;
+case 1023:
+  Serial.println("Down1:");
+  break;
+}
+
+if (digitalRead(2) == LOW) {
+Serial.println("Enter:");
+}
+delay(50);
 }
